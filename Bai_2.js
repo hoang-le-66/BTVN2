74024062088bc7cc1032e236d3c9e6572a02a387
hoang-le-66 <leluuhoang6601@gmail.com>
2022-09-20 17:10:35 +0700

    xoa

--- Bai_2.js
+++
@@ -1,4 +0,0 @@
-let n = 10;
-for (let index = 1; index <= n; index+=2) {
-    console.log(index);
-}
--- Bai_6.js
+++
@@ -1,6 +0,0 @@
-let n =3 
-let sum = 0 ; 
-for (let index = 1; index <=n; index++) {
-    sum+= Math.pow(index,2);
-}
-console.log(sum);
